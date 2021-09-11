565447b1bf6ffbee820ec8e878cf707c11232729
Aleksandr <legionerab3000@yandex.ru>
2021-09-11 14:57:15 +0300

    Исправил ошибку с подключением слайдера

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,16 @@
-'use strict';
-
-import slider from './modules/slider';
+"use strict";
 
 // Скрипт начнёт выполняться после полной загрузки контента
-document.addEventListener('DOMContentLoaded', () => {
-
-    // Инициализируем слайдер
-    slider({
-        container: '.slider',
-        slide: '.slider__slide',
-        nextBtn: '.slider__next',
-        prevBtn: '.slider__prev',
-        totalCounter: '.slider__total',
-        currentCounter: '.slider__current',
-        wrapper: '.slider__wrapper',
-        carousel: '.slider__carousel'
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  // Инициализируем слайдер
+  slider.slider({
+    container: ".slider",
+    slide: ".slider__slide",
+    nextBtn: ".slider__next",
+    prevBtn: ".slider__prev",
+    totalCounter: ".slider__total",
+    currentCounter: ".slider__current",
+    wrapper: ".slider__wrapper",
+    carousel: ".slider__carousel",
+  });
 });
-
-
